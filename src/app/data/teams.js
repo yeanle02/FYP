ba60e2108a2333c7fc3a88c8e97440d3a37cdf6b
Ekramul Islam <ekramjim002@gmail.com>
2025-03-18 03:27:48 +1100

    teams data hardcoded (sample)

---
+++ src/app/data/teams.js
@@ -0,0 +1,68 @@
+export const teams = [
+  {
+    name: "Collingwood",
+    logo: "/globe.svg",
+    stats: {
+      wins: 4,
+      losses: 1,
+      draws: 0,
+      percentage: 128.5,
+      points: 16
+    }
+  },
+  {
+    name: "Carlton",
+    logo: "/globe.svg",
+    stats: {
+      wins: 4,
+      losses: 1,
+      draws: 0,
+      percentage: 125.8,
+      points: 16
+    }
+  },
+  {
+    name: "Melbourne",
+    logo: "/globe.svg",
+    stats: {
+      wins: 4,
+      losses: 1,
+      draws: 0,
+      percentage: 122.3,
+      points: 16
+    }
+  },
+  {
+    name: "Brisbane Lions",
+    logo: "/globe.svg",
+    stats: {
+      wins: 3,
+      losses: 2,
+      draws: 0,
+      percentage: 118.7,
+      points: 12
+    }
+  },
+  {
+    name: "Sydney",
+    logo: "/globe.svg",
+    stats: {
+      wins: 3,
+      losses: 2,
+      draws: 0,
+      percentage: 115.2,
+      points: 12
+    }
+  },
+  {
+    name: "Western Bulldogs",
+    logo: "/globe.svg",
+    stats: {
+      wins: 3,
+      losses: 2,
+      draws: 0,
+      percentage: 110.5,
+      points: 12
+    }
+  }
+];
