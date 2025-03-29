e3d40d563a290073191bc95e84740756b908551d
Kaixin0322 <kxuu0036@student.monash.edu>
2025-03-29 12:24:35 +1100

    move components to components

--- src/app/teams/page.js
+++ src/app/teams/page.js
@@ -1,37 +1,43 @@
 "use client";
 
 import { Navbar } from "@/components/Navbar";
-import TeamHeader from "./TeamHeader";
-import TeamInfoCard from "./TeamInfoCard";
-import MatchHistory from "./MatchHistory";
+import TeamHeader from "@/components/TeamHeader";
+import TeamInfoCard from "@/components/TeamInfoCard";
+import MatchHistory from "@/components/MatchHistory";
 
-export default function Home() {
+export default function TeamsPage() {
   return (
     <main className="min-h-screen bg-gray-100">
       <Navbar />
+      
       <div className="container mx-auto px-4 py-6">
-        <TeamHeader teamName="Sydney Swans" homeVenue="SCG" />
+        <div className="max-w-4xl mx-auto">
+          <TeamHeader teamName="Sydney Swans" homeVenue="SCG" />
+        </div>
       </div>
 
-      <div className="container mx-auto px-4 flex gap-8">
-        <div className="w-[360px] flex flex-col gap-6" style={{ marginLeft: "160px" }}>
-          <TeamInfoCard
-            role="Captain"
-            name="Callum Mills"
-            imageSrc="/teams/Sydney_Swans.png"
-            link="#"
-          />
-          <TeamInfoCard
-            role="Coach"
-            name="John Longmire"
-            imageSrc="/teams/sydney-coach.png"
-            link="#"
-          />
-        </div>
-        <div className="flex-1 pr-3">
-          <MatchHistory />
+      <div className="container mx-auto px-4 py-4">
+        <div className="max-w-4xl mx-auto flex gap-8">
+          <div className="w-[360px] flex flex-col gap-6">
+            <TeamInfoCard
+              role="Captain"
+              name="Callum Mills"
+              imageSrc="/teams/Sydney_Swans.png"
+              link="#"
+            />
+            <TeamInfoCard
+              role="Coach"
+              name="John Longmire"
+              imageSrc="/teams/sydney-coach.png"
+              link="#"
+            />
+          </div>
+          <div className="flex-1 pr-3">
+            <MatchHistory />
+          </div>
         </div>
       </div>
+      
     </main>
   );
 }
